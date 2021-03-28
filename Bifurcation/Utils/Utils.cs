da6d8163bf9ffb257c43a680b7e2cd73f7e4f9af
Festino <offestashka@mail.ru>
2021-03-28 22:47:24 +0300

    Reorganized

--- Bifurcation/Utils/Utils.cs
+++ Bifurcation/Utils/Utils.cs
@@ -9,17 +9,26 @@ namespace Bifurcation
         {
             return new ColumnDefinition() { Width = new GridLength(value, GridUnitType.Star) };
         }
-        public static RowDefinition RowStarDefinition(double value)
-        {
-            return new RowDefinition() { Height = new GridLength(value, GridUnitType.Star) };
-        }
         public static ColumnDefinition ColumnPixelDefinition(double value)
         {
             return new ColumnDefinition() { Width = new GridLength(value, GridUnitType.Pixel) };
         }
+        public static ColumnDefinition ColumnAuto()
+        {
+            return new ColumnDefinition() { Width = new GridLength(0, GridUnitType.Auto) };
+        }
+
+        public static RowDefinition RowStarDefinition(double value)
+        {
+            return new RowDefinition() { Height = new GridLength(value, GridUnitType.Star) };
+        }
         public static RowDefinition RowPixelDefinition(double value)
         {
             return new RowDefinition() { Height = new GridLength(value, GridUnitType.Pixel) };
+        }
+        public static RowDefinition RowAuto()
+        {
+            return new RowDefinition() { Height = new GridLength(0, GridUnitType.Auto) };
         }
     }
 }
